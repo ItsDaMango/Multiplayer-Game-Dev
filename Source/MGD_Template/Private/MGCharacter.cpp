04e19c887ee2c1f11a95282ae1dbf052ab5fedd9
ItsDaMango <wright.e.will@gmail.com>
2024-05-28 14:57:44 +1000

    MG Week 1
    Set up rider and uproject, create basic movement functions in cpp

---
+++ Source/MGD_Template/Private/MGCharacter.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MGCharacter.h"
+
+
+// Sets default values
+AMGCharacter::AMGCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AMGCharacter::Pure_MoveCharacter(const FVector2D Axis)
+{
+	// update the member axis
+	pMoveAxis = Axis;
+
+	// move character based on Y Axis
+	AddMovementInput(GetControlRotation().Vector(), Axis.Y);
+
+	// move character based on X Axis
+	AddMovementInput(FRotationMatrix(GetControlRotation()).GetScaledAxis(EAxis::Y), Axis.X);
+	
+}
+
+void AMGCharacter::Pure_RotateController(const FVector2D Axis)
+{
+	// rotate controller Yaw based on X Axis
+	AddControllerYawInput(Axis.X);
+
+	// rotate controller Pitch based on Y Axis
+	AddControllerPitchInput(-Axis.Y);
+
+	
+}
+
---
+++ Source/MGD_Template/Public/MGCharacter.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "MGCharacter.generated.h"
+
+UCLASS()
+class MGD_TEMPLATE_API AMGCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AMGCharacter();
+
+	// move the character based on an axis
+	UFUNCTION(BlueprintCallable, Category="Movement")
+	void Pure_MoveCharacter(const FVector2D Axis);
+
+	// rotate the character controller based on axis
+	UFUNCTION(BlueprintCallable, Category="Controller")
+	void Pure_RotateController(const FVector2D Axis);
+	
+public:
+	// move axis of the character
+	UPROPERTY(BlueprintReadOnly, Category="Movement")
+	FVector2D pMoveAxis; 
+};
